24be9500c0c30e663124f0ab16c5dfdfbd3c648f
rom <romvargas81@gmail.com>
2019-05-13 13:21:30 -0500

    se agregan meta etiquetas para responsive y archivo de configuracion manifest.json

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
     console.log("Hola JQuery");
+
+    $("#menu a").click(function(e) {
+        e.preventDefault(); //previene el link a pagina anterior
+
+        $("html, body").animate({
+            scrollTop: $($(this).attr('href')).offset().top //para conceguir la posiscion del elemento
+        });
+        return false;
+    });
 });
