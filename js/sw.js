b5c07e3d7ff3bc6cb1ac185b895b4a9d9de2d01d
rom <romvargas81@gmail.com>
2019-05-14 09:57:51 -0500

    se agrega variable cache a serviceworker

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,20 @@
+// asignar nombre y version de cache
+const CACHE_NAME = 'v1_cache_app_pwa';
+
+//Ficeheros a cachar en la app - se deben cachear todos los archivos e iconos en la app
+var urlsToCache = [
+    './',
+    './css/styles.css',
+    './img/avatarRom.png',
+    './img/dataMinning.png',
+    './img/facebook.png',
+    './img/hack.png',
+    './img/instagram.png',
+    './img/twitter.png',
+];
+
+// evento install
+// evento que se encarga de instalar y guardar en cache los servicios estaticos
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME)
